7627eaedfbbc9b870467fa6d9622295af121f7b6
Johannes Moersch <Johannes.Moersch@gmail.com>
2016-01-30 17:00:38 -0600

    UI Updates!

---
+++ Assets/Scripts/CultistUIScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CultistUIScript : MonoBehaviour
+{
+	public Text CultistName;
+
+	public Text CultistProfession;
+
+	public Text CultistTraits;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+}
--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -1,25 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class UIScript : MonoBehaviour
 {
 	public GameObject Panel1;
+	public GameObject Panel2;
 
 	public GameObject ButtonPrefab;
 
+	public CultistUIScript CultistUIScript;
+
 	void Start()
 	{
-		for (int i = 0; i < 10; ++i)
-		{
-			var newButton = GameObject.Instantiate(ButtonPrefab);
-			newButton.GetComponent<RectTransform>().position += new Vector3(0, i * -30, 0);
-			newButton.transform.parent = Panel1.transform;
-		}
-		Panel1.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 10 * 30);
+		SetButtons(Panel1, new[] { "Hello", "Cruel", "World" });
+		SetButtons(Panel2, new[] { "Hello", "Cruel", "World" });
 	}
 
 	void Update()
 	{
 
+	}
+
+	private void SetButtons(GameObject panel, string[] text)
+	{
+		foreach (Transform transform in panel.transform)
+			Destroy(transform);
+		for (int i = 0; i < text.Length; ++i)
+		{
+			var newButton = GameObject.Instantiate(ButtonPrefab);
+			newButton.GetComponent<RectTransform>().position += new Vector3(0, i * -30, 0);
+			newButton.transform.SetParent(panel.transform, false);
+			newButton.GetComponentInChildren<Text>().text = text[i];
+
+			var index = i;
+			newButton.GetComponent<Button>().onClick.AddListener(() => Debug.Log(text[index] + " Pressed"));
+		}
+		panel.GetComponent<RectTransform>().sizeDelta = new Vector2(0, text.Length * 30);
 	}
 }
