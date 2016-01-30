79b4259a0a828d9c653322943a3a1dca7075746c
alexander.flegel <alexander.flegel@omnilogic.net>
2016-01-30 11:29:56 -0600

    actually added the models this time

---
+++ CultSimulator2016/Assets/Standard Assets/Models/Sacrifice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Standard_Assets.Models
+{
+	class Sacrifice
+	{
+		public decimal abduction { get; set; }
+		public decimal abductionDefense { get; set; }
+		public decimal investigation { get; set; }
+		public decimal investigationDefense { get; set; }
+		public decimal recruitment { get; set; }
+		public decimal recruitmentDefense { get; set; }
+		public ProfessionTrait.professions profession;
+	}
+}
---
+++ CultSimulator2016/Assets/Standard Assets/Models/Trait.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Standard_Assets.Models
+{
+	class Trait
+	{
+
+		public Dictionary<KeyValuePair<ProfessionTrait.traits, ProfessionTrait.traits>, int> traitMapping;
+
+
+		public int getValue(ProfessionTrait.traits val1, ProfessionTrait.traits val2)
+		{
+
+			return 0;
+		}
+
+		public void initialize()
+		{
+
+		}
+
+	}
+}
