3a733ff2ea31f5cea518b8909d896a78dd56cd46
alexander.flegel <alexander.flegel@omnilogic.net>
2016-01-30 15:37:42 -0600

    updated trait pool

--- Assets/CultSimulator/TraitPool.cs
+++ Assets/CultSimulator/TraitPool.cs
@@ -8,35 +8,56 @@ namespace Assets.CultSimulator
 {
 	class TraitPool
 	{
-		public Dictionary<Sin, Dictionary<Sin, int>> traitPool;
+		public Dictionary<TraitMap, int> traitPool;
 
 		public TraitPool()
 		{
-			traitPool = new Dictionary<Sin, Dictionary<Sin, int>>();
+			traitPool = new Dictionary<TraitMap, int>();
 
 		}
+
+
 
 		public int GetTraitValue(SearchableAsset keyValues, SearchableAsset matchValues)
 		{
 			int fullValue = 0;
 			//loop through every trait against every other trait and return a full sum
-			//			fullValue += GetTraitValue(trait, match);
+			fullValue += GetTraitValue(keyValues.sin, matchValues.sin);
+			fullValue += GetTraitValue(keyValues.sin, matchValues.virtue);
+			fullValue += GetTraitValue(keyValues.virtue, matchValues.sin);
+			fullValue += GetTraitValue(keyValues.virtue, matchValues.virtue);
 
 			return fullValue;
 		}
 
-		public int GetTraitValue(Sin keyValue, Sin matchValue)
+		public int GetTraitValue(object keyValue, object matchValue)
 		{
+			TraitMap getKey = new TraitMap(keyValue, matchValue);
+
 			//check the dictionary against values and return the match
-			if (traitPool.ContainsKey(keyValue))
-				return traitPool[keyValue].ContainsKey(matchValue) ? traitPool[keyValue][matchValue] : 0;
-			else
-				return traitPool[matchValue].ContainsKey(keyValue) ? traitPool[matchValue][keyValue] : 0;
+			return traitPool[getKey];
 		}
 	}
 
 	struct TraitMap
 	{
+		object firstKey;
+		object secondKey;
 
+		public TraitMap(object key1, object key2)
+		{
+			firstKey = key1;
+			secondKey = key2;
+		}
+
+		public override int GetHashCode()
+		{
+			return firstKey.GetHashCode() + secondKey.GetHashCode();
+		}
+
+		public override bool Equals(object obj)
+		{
+			return Equals(firstKey, secondKey);
+		}
 	}
 }
