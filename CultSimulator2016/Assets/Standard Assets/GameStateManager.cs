d90e165da003fc5f49f4b9164ff09e58e1f8d244
Chris Dusyk <chris.dusyk@gmail.com>
2016-01-30 12:06:06 -0600

    Added YearTarget.cs and worked on GameStateManager

--- CultSimulator2016/Assets/Standard Assets/GameStateManager.cs
+++ CultSimulator2016/Assets/Standard Assets/GameStateManager.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using Assets.Standard_Assets.Models;
 
 namespace Assets.Standard_Assets
 {
-	class GameStateManager : MonoBehaviour
+	public class GameStateManager : MonoBehaviour
 	{
-
+		 public YearTarget CurrentTarget { get; private set; }
 	}
 }
---
+++ CultSimulator2016/Assets/Standard Assets/Models/YearTarget.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Standard_Assets.Models
+{
+	public class YearTarget
+	{
+		public int NumberOfCultists { get; private set; }
+		public int[] SacrificeTargets { get; private set; }
+	}
+}
