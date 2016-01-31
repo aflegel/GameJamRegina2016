011df77014db0b8d9bb0529dc922c93dd7628f50
Chris Dusyk <chris.dusyk@gmail.com>
2016-01-30 20:33:29 -0600

    Began work on YearTargetFactory

--- Assets/CultSimulator/YearTarget.cs
+++ Assets/CultSimulator/YearTarget.cs
@@ -10,4 +10,22 @@ namespace Assets.CultSimulator
 		public int NumberOfCultists { get; set; }
 		public SearchableAsset[] SacrificeTargets { get; set; }
 	}
+
+	public static class YearTargetFactory
+	{
+		public static YearTarget GetNextYearTargets(int yearNumber)
+		{
+			var nextYearTargets = new YearTarget();
+			Random randomNumber = new Random();
+
+			switch (yearNumber)
+			{
+				case 1:
+
+					break;
+			}
+
+			return nextYearTargets;
+		}
+	}
 }
